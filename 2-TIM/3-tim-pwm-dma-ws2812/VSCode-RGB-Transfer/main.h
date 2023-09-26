2d62b0a080aac9275b9036a33c484b8a2cdc8aa5
liuao <2494210546@qq.com>
2023-09-26 11:47:09 +0800

    2023.9.26

---
+++ 2-TIM/3-tim-pwm-dma-ws2812/VSCode-RGB-Transfer/main.h
@@ -0,0 +1,57 @@
+/*
+ * @Author: liuao 2494210546@qq.com
+ * @Date: 2023-09-26 11:24:29
+ * @LastEditors: liuao 2494210546@qq.com
+ * @LastEditTime: 2023-09-26 11:46:10
+ * @FilePath: \VSCode-RGB-Transfer\main.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+/*  https://blog.csdn.net/neo_Wordsworth/article/details/120622519   */
+#define     WS_WHITE        0xFFFFFF
+#define     WS_RED          0XFF0000
+#define     WS_RGREEN       0X00FF00
+#define     WS_BLUE         0X0000FF
+
+#define     WS_ORANGE       0XF28500     /* 橙色 */
+
+#define     WS_IvyGreen     0X36BF36    /* 常春藤绿 */
+#define     ForestGreen     0X228B22    /* 森林绿 */
+#define     LimeGreen       0X32CD32    /* 柠檬绿 */
+#define     DarkGreen       0X006400    /* 暗绿 */
+#define     Lime            0X00FF00    /* 鲜绿色 */
+#define     Malachite       0X22C32E    /* 孔雀石绿 */
+
+
+#define     SalviaBlue      0X4D80E6    /* 尾草蓝 */
+#define     DarkPowder      0X003399    /* 暗婴儿粉蓝 */
+#define     Sapphire        0X082567    /* 蓝宝石色 */
+#define     International   0X002FA7    /* 国际奇连蓝 */
+#define     Ceruleanblue    0X2A52BE    /* 蔚蓝 */
+#define     RoyalBlue       0X4169E1    /* 品蓝 */
+#define     DarkMineralBlue 0X24367D    /* 暗矿蓝 */
+#define     Ultramarine     0X0033FF    /* 极浓海蓝 */
+#define     LapisLazuli     0X0D33FF    /* 天青石蓝 */
+
+
+
+#define     Purple              0X800080    /* 紫色 */
+#define     DarkMagenta         0X8B008B    /* 暗洋红 */
+#define     Magenta             0XFF00FF    /* 洋红 */
+#define     Fuchsia             0XF400A1    /* 品红 */
+#define     Orchid              0XDA70D6    /* 兰紫 */
+#define     PearlPink           0XFFB3E6    /* 浅珍珠红 */
+#define     OldRose             0XB85798    /* 陈玫红 */
+#define     RosePink            0XFF66CC    /* 浅玫瑰红 */
+#define     MediumVioletRed     0XC71585    /* 中青紫红 */
+#define     MagentaRose         0XFF0DA6    /* 洋玫瑰红 */
+#define     Rose                0XFF007F    /* 玫瑰红 */
+#define     Ruby                0XCC0080    /* 红宝石色 */
+#define     Camellia            0XE63995    /* 山茶红 */
+#define     DeepPink            0XFF1493    /* 深粉红 */
+#define     Flamingo            0XE68AB8    /* 火鹤红 */
+#define     CoralPink           0XFF80BF    /* 浅珊瑚红 */
+
+
+
